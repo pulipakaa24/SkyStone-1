b553a71c878628e1f005fc0474c1b43f76047805
Naren R <nwolfram49@gmail.com>
2019-11-08 16:09:28 -0600

    real auton things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BernardAutoBlue1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BernardAutoBlue1.java
@@ -12,7 +12,7 @@ public class BernardAutoBlue1 extends TestDriving {
         int moveAmount = 0;
         super.runOpMode();
         robot.servoTwist.setPosition(0);
-        robot.servoGrab.setPosition(-45);
+        robot.servoGrab.setPosition(0);
         robot.servoLatch.setPosition(1);
         encoderDrive(.3,-30,-30,20);
         while (robot.distSensor.getDistance(DistanceUnit.INCH) > 4.5)
@@ -22,21 +22,21 @@ public class BernardAutoBlue1 extends TestDriving {
         }
         //((robot.sensorColor.alpha() + robot.sensorColor2.alpha())/2)>150
         boolean found = false;
-        sleep(100);
+        sleep(500);
         while (((robot.sensorColor.alpha() + robot.sensorColor2.alpha())/2) > 80)
         {
-            robot.motorBack.setPower(.3);
-            robot.motorFront.setPower(.3);
+            robot.motorBack.setPower(.06);
+            robot.motorFront.setPower(.09);
             moveAmount++;
-            sleep(300);
+            sleep(1000);
         }
         robot.motorFront.setPower(0);
         robot.motorFront.setPower(0);
         //while(robot.sensorColor.red() > robot.sensorColor.blue() + 15 && robot.sensorColor.green() > robot.sensorColor.blue() + 15)//this tests if a block is yellow. If you run this where there is no block, it'll likely assume that it is a skystone
-        robot.servoGrab.setPosition(90);
+        robot.servoGrab.setPosition(1);
         telemetry.addData("Color:","skystone");
         encoderDrive(1, 3,  3, 5);
-        gyroDrive(-90, "z", 1, 5, false);
+        gyroDrive(90, "z", -1, 5, false);
 
 
         encoderDrive(1, -60, -60, 10);
