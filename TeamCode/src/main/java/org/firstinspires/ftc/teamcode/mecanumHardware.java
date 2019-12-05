3127d6a55331c01bdde7d698deb5cb4a2903b5b3
schroederr22 <schroederr22@greenhill.org>
2019-12-05 16:30:21 -0600

    Brought in vuforia and unscrewed everything up.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumHardware.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class mecanumHardware
+{
+    // Here we define all of the motors we use.
+    // Our robot uses omni-wheels and a four motor drive system.
+    public DcMotor motorFrontLeft;
+    public DcMotor motorBackRight;
+    public DcMotor motorBackLeft;
+    public DcMotor motorFrontRight;
+    public Servo servoDrag;
+//    public DcMotor leftLift;
+//    public DcMotor rightLift;
+
+
+
+
+
+
+    // Define hardware map
+    HardwareMap hwMap;
+
+    // Initialize standard hardware interfaces
+    public void init(HardwareMap ahwMap) {
+
+        hwMap = ahwMap;
+
+        // Here we name the motors.
+        motorFrontLeft  = hwMap.get(DcMotor.class, "motorFrontLeft");
+
+
+        motorBackRight = hwMap.get(DcMotor.class, "motorBackRight");
+
+
+        motorBackLeft  = hwMap.get(DcMotor.class, "motorBackLeft");
+
+
+        motorFrontRight  = hwMap.get(DcMotor.class, "motorFrontRight");
+
+        servoDrag = hwMap.get(Servo.class, "servoDrag");
+//        leftLift  = hwMap.get(DcMotor.class, "leftLift");
+//
+//
+//        rightLift  = hwMap.get(DcMotor.class, "rightLift");
+
+
+
+        //90 is up | 0 is down
+
+
+
+        // Here we set the motor directions.
+        motorFrontRight.setDirection(DcMotor.Direction.FORWARD);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+//        leftLift.setDirection(DcMotor.Direction.FORWARD);
+//        rightLift.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+        // Here we set the servo directions.
+
+        // Here we set all motors to zero power for safety.
+        motorFrontRight.setPower(0);
+        motorBackLeft.setPower(0);
+        motorFrontLeft.setPower(0);
+        motorBackRight.setPower(0);
+        servoDrag.setPosition(.9);
+//        leftLift.setPower(0);
+//        rightLift.setPower(0);
+
+
+        // Here we set all motors to run with encoders.
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        leftLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        rightLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
+}
