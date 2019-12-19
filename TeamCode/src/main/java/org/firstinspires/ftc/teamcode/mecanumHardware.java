34015d2338e5d2c348c560c6006a04268fa7b483
aariandhanani <aariandhanani@gmail.com>
2019-12-19 12:36:53 -0600

    Merge branch 'master' of https://github.com/Viridian-Roboics/SkyStone into ryanIsTheBestProgrammerOnThisTeam
    # Conflicts:
    #	FtcRobotController/src/main/AndroidManifest.xml
    #	TeamCode/src/main/AndroidManifest.xml
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaSkyStoneNavigationWebcam.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumHardware.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumTeleOp.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumHardware.java
@@ -16,10 +16,7 @@ public class mecanumHardware
     public DcMotor motorBackRight;
     public DcMotor motorBackLeft;
     public DcMotor motorFrontRight;
-    public DcMotor motorLiftR;
-    public DcMotor motorLiftL;
-    public Servo servoGrab;
-    //public Servo servoDrag;
+    public Servo servoDrag;
 //    public DcMotor leftLift;
 //    public DcMotor rightLift;
 
@@ -48,11 +45,7 @@ public class mecanumHardware
 
         motorFrontRight  = hwMap.get(DcMotor.class, "motorFrontRight");
 
-        motorLiftR = hwMap.get(DcMotor.class,"motorLiftRight");
-
-        motorLiftL = hwMap.get(DcMotor.class, "motorLiftLeft");
-
-        //servoDrag = hwMap.get(Servo.class, "servoDrag");
+        servoDrag = hwMap.get(Servo.class, "servoDrag");
 //        leftLift  = hwMap.get(DcMotor.class, "leftLift");
 //
 //
@@ -62,17 +55,13 @@ public class mecanumHardware
 
         //90 is up | 0 is down
 
-        servoGrab = hwMap.get(Servo.class, "servoGrab");
-
 
 
         // Here we set the motor directions.
         motorFrontRight.setDirection(DcMotor.Direction.FORWARD);
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
-        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotor.Direction.FORWARD);
-        motorLiftL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorLiftR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
 //        leftLift.setDirection(DcMotor.Direction.FORWARD);
 //        rightLift.setDirection(DcMotor.Direction.REVERSE);
 
@@ -85,11 +74,7 @@ public class mecanumHardware
         motorBackLeft.setPower(0);
         motorFrontLeft.setPower(0);
         motorBackRight.setPower(0);
-        motorLiftR.setPower(0);
-        motorLiftL.setPower(0);
-
-        servoGrab.setPosition(0.5);
-        //servoDrag.setPosition(.9);
+        servoDrag.setPosition(.9);
 //        leftLift.setPower(0);
 //        rightLift.setPower(0);
 
@@ -99,8 +84,6 @@ public class mecanumHardware
         motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorLiftR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorLiftL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 //        leftLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        rightLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
