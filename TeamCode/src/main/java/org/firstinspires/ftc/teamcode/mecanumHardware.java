827e50f0064f76bbb056ddd1ab079cd8c48b4174
DrakeDemolishr <43097520+DrakeDemolishr@users.noreply.github.com>
2019-12-19 14:37:21 -0600

    commit to see if it will let me run

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumHardware.java
@@ -16,7 +16,11 @@ public class mecanumHardware
     public DcMotor motorBackRight;
     public DcMotor motorBackLeft;
     public DcMotor motorFrontRight;
-    public Servo servoDrag;
+    public DcMotor motorLiftR;
+    public DcMotor motorLiftL;
+    public Servo servoGrab;
+    public Servo claw;
+    //public Servo servoDrag;
 //    public DcMotor leftLift;
 //    public DcMotor rightLift;
 
@@ -45,14 +49,10 @@ public class mecanumHardware
 
         motorFrontRight  = hwMap.get(DcMotor.class, "motorFrontRight");
 
-<<<<<<< HEAD
-        servoDrag = hwMap.get(Servo.class, "servoDrag");
-=======
         motorLiftR = hwMap.get(DcMotor.class,"motorLiftRight");
         motorLiftL = hwMap.get(DcMotor.class, "motorLiftLeft");
 
         //servoDrag = hwMap.get(Servo.class, "servoDrag");
->>>>>>> parent of a336b02... Added zero power behavior
 //        leftLift  = hwMap.get(DcMotor.class, "leftLift");
 //
 //
@@ -62,18 +62,16 @@ public class mecanumHardware
 
         //90 is up | 0 is down
 
+        servoGrab = hwMap.get(Servo.class, "servoGrab");
+        claw = hwMap.get(Servo.class, "claw");
+
 
 
         // Here we set the motor directions.
         motorFrontRight.setDirection(DcMotor.Direction.FORWARD);
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
-<<<<<<< HEAD
-        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
-        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
-=======
         motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.FORWARD);
->>>>>>> parent of a336b02... Added zero power behavior
 //        leftLift.setDirection(DcMotor.Direction.FORWARD);
 //        rightLift.setDirection(DcMotor.Direction.REVERSE);
 
@@ -86,7 +84,12 @@ public class mecanumHardware
         motorBackLeft.setPower(0);
         motorFrontLeft.setPower(0);
         motorBackRight.setPower(0);
-        servoDrag.setPosition(.9);
+        motorLiftR.setPower(0);
+        motorLiftL.setPower(0);
+
+        servoGrab.setPosition(0.5);
+        claw.setPosition(.5);
+        //servoDrag.setPosition(.9);
 //        leftLift.setPower(0);
 //        rightLift.setPower(0);
 
@@ -96,6 +99,8 @@ public class mecanumHardware
         motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLiftR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLiftL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 //        leftLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        rightLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
