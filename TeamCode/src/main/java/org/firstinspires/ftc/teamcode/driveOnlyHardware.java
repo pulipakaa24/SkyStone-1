a91fa0686f194bd8381e320a78eecd9a29c8d8d5
schroederr22 <schroederr22@greenhill.org>
2020-01-23 13:36:23 -0600

    Added Drive only classes to practice whilst builders are building.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOnlyHardware.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class driveOnlyHardware
+{
+    // Here we define all of the motors we use.
+    // Our robot uses omni-wheels and a four motor drive system.
+    public DcMotor motorFrontLeft;
+    public DcMotor motorBackRight;
+    public DcMotor motorBackLeft;
+    public DcMotor motorFrontRight;
+//    public DcMotor motorLiftR;
+//    public DcMotor motorLiftL;
+//    public Servo servoLatch;
+//    public CRServo servoClaw;
+//    public Servo servoTwist;
+
+    //public Servo servoDrag;
+//    public DcMotor leftLift;
+//    public DcMotor rightLift;
+
+
+
+
+
+
+    // Define hardware map
+    HardwareMap hwMap;
+
+    // Initialize standard hardware interfaces
+    public void init(HardwareMap ahwMap) {
+
+        hwMap = ahwMap;
+
+        // Here we name the motors.
+        motorFrontLeft  = hwMap.get(DcMotor.class, "motorFrontLeft");
+
+
+        motorBackRight = hwMap.get(DcMotor.class, "motorBackRight");
+
+
+        motorBackLeft  = hwMap.get(DcMotor.class, "motorBackLeft");
+
+
+        motorFrontRight  = hwMap.get(DcMotor.class, "motorFrontRight");
+
+//        motorLiftR = hwMap.get(DcMotor.class,"motorLiftR");
+//
+//        motorLiftL = hwMap.get(DcMotor.class, "motorLiftL");
+
+        //servoDrag = hwMap.get(Servo.class, "servoDrag");
+//        leftLift  = hwMap.get(DcMotor.class, "leftLift");
+//
+//
+//        rightLift  = hwMap.get(DcMotor.class, "rightLift");
+
+
+
+        //90 is up | 0 is down
+
+//        servoLatch = hwMap.get(Servo.class, "servoLatch");
+//        servoClaw = hwMap.get(CRServo.class, "servoClaw");
+//        servoTwist = hwMap.get(Servo.class,"servoTwist");
+////        servoLatch.scaleRange(.5,1);
+//        servoLatch.setDirection(Servo.Direction.FORWARD);
+////        servoClaw.scaleRange(.5,1);
+//        servoClaw.setDirection(CRServo.Direction.FORWARD);
+////        servoTwist.scaleRange(.5,1)
+//        servoTwist.setDirection(Servo.Direction.REVERSE);
+
+
+
+
+        // Here we set the motor directions.
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+//        motorLiftL.setDirection(DcMotor.Direction.FORWARD);
+//        motorLiftR.setDirection(DcMotor.Direction.FORWARD);
+//        leftLift.setDirection(DcMotor.Direction.FORWARD);
+//        rightLift.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+        // Here we set the servo directions.
+
+        // Here we set all motors to zero power for safety.
+        motorFrontRight.setPower(0);
+        motorBackLeft.setPower(0);
+        motorFrontLeft.setPower(0);
+        motorBackRight.setPower(0);
+//        motorLiftR.setPower(0);
+//        motorLiftL.setPower(0);
+//
+//        servoLatch.setPosition(-0.1);
+//        //servoClaw.setPosition(-.8);
+        //servoTwist.setPosition(1);
+        //servoDrag.setPosition(.9);
+//        leftLift.setPower(0);
+//        rightLift.setPower(0);
+
+
+        // Here we set all motors to run with encoders.
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        motorLiftR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        motorLiftL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        motorLiftR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        motorLiftL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        leftLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        rightLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOnlyTeleOp.java
@@ -0,0 +1,209 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="DriveOnlyTeleOp", group="Zippo")
+//@Disabled
+
+//THE KEY BELOW IS FOR USING IMAGE RECOGNITION -PULKIT
+//AT5CUvf/////AAAAGaBn6TlejU79iRr5dpGz0Msa4+WbMquS0c0rHQGMURBOGIxPznOmaavjYRYfWHE/qRnpaHDvKIVV1drOmZguwKjiTVfUzVpkRgxdFzcVDsNBldxzhrcSl+bRKGlNv3zKHDfaOJioTa7uzIN/uKUzdJPX+o5PQQxRPYXBuIvAkASbZ9/MVjq5u3Jltyw3Gz9DCPVgxqcMKILOwv9FpMDMRTcgeRwk7f+pPd8f5FmB8ehr3xiDbPxydmYAkpuqQ6Mx2qiggbSlzl4uTm2JeqOP3hbej+ozcevtHKh9C4S3eKodfDUpKekBfdOuR2aer0FwrWxfAqmdOewy5Tei71lLAOgEzx+vo6OPKpSzbTh1gFzI
+
+public class driveOnlyTeleOp extends OpMode{
+
+    mecanumHardware robot  = new mecanumHardware();
+
+    // Create variables for motor power
+    private double flPower = 0; //left wheel
+    private double frPower = 0; //right wheel
+    private double blPower = 0; //front power
+    private double brPower = 0; //back power
+    private boolean scalePower = false;
+    private boolean cornerTurn = true;
+    private boolean aCheck = false;
+//    private double llpower = 0; // left lift power
+//    private double rlpower = 0; // right lift power
+
+
+
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+        //robot.sensorColor.enableLed(false);
+        double time =  System.currentTimeMillis();
+//        robot.servoLatch.setPosition(-0.1);
+
+    }
+
+    @Override
+    public void loop() {
+//      MAIN DRIVING CONTROLS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//        lPower = gamepad1.right_stick_y;
+//        rPower = gamepad1.right_stick_y;
+//        fPower = -gamepad1.right_stick_x;
+//        bPower = -gamepad1.right_stick_x;
+        if ((gamepad1.left_stick_y > 0.1 || gamepad1.left_stick_y < -0.1)||(gamepad2.left_stick_y > 0.1 || gamepad2.left_stick_y < -0.1))
+        {
+            flPower = gamepad1.left_stick_y * 2;
+            brPower = gamepad1.left_stick_y *2;
+            frPower = gamepad1.left_stick_y *2;
+            blPower = gamepad1.left_stick_y* 2;
+        }
+
+
+        else if ((gamepad1.left_stick_x < -0.2 || gamepad1.left_stick_x > .2) || (gamepad2.left_stick_x <-.02 || gamepad2.left_stick_x > .2))
+        {
+            frPower = gamepad1.left_stick_x * 2;
+            blPower = gamepad1.left_stick_x * 2;
+            flPower = -gamepad1.left_stick_x * 2;
+            brPower = -gamepad1.left_stick_x * 2;
+        }
+        else
+        {
+            flPower = 0;
+            brPower = 0;
+            frPower = 0;
+            blPower = 0;
+        }
+
+//           //rotate
+
+        if (true) {
+            if (gamepad1.right_stick_x < -0.1 || gamepad2.right_stick_x < -0.1) {
+                flPower = 1;
+                brPower = -1;
+                frPower = -1;
+                blPower = 1;
+            } else if (gamepad1.right_stick_x > 0.1 || gamepad2.right_stick_x> 0.1) {
+                flPower = -1;
+                brPower = 1;
+                frPower = 1;
+                blPower = -1;
+            }
+        }
+        else
+        {
+            if (gamepad1.right_stick_x > 0.1) {
+                flPower = -1;
+
+                frPower = -1;
+                blPower = 1;
+            } else if (gamepad1.right_stick_x < -0.1) {
+                flPower = 1;
+                brPower = -1;
+                blPower = -1;
+
+            }
+        }
+
+
+        if(frPower > 1)
+        {
+            frPower = 1;
+        }
+        else if (frPower < -1)
+        {
+            frPower = -1;
+        }
+
+        if(flPower > 1)
+        {
+            flPower = 1;
+        }
+        else if (flPower < -1)
+        {
+            flPower = -1;
+        }
+
+        if(blPower > 1)
+        {
+            blPower = 1;
+        }
+        else if (blPower < -1)
+        {
+            blPower = -1;
+        }
+        if(brPower > 1)
+        {
+            brPower = 1;
+        }
+        else if (brPower < -1)
+        {
+            brPower = -1;
+        }
+//
+        if (gamepad1.left_trigger > .1)
+        {
+            scalePower = true;
+        }
+        else
+        {
+            scalePower = false;
+        }
+        if (gamepad1.right_trigger > .1)
+        {
+            cornerTurn = true;
+        }
+        else
+        {
+            cornerTurn = false;
+        }
+        //BringDown hook = new BringDown();
+
+
+//      SCALING POWERS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        if(scalePower)
+        {
+            flPower *= .3;
+            brPower *= .3;
+            frPower *= .3;
+            blPower *= .3;
+        }
+
+//      SETTING POWERS AND POSITIONS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        robot.motorFrontLeft.setPower(flPower);
+        robot.motorBackRight.setPower(brPower);
+        robot.motorFrontRight.setPower(frPower);
+        robot.motorBackLeft.setPower(blPower);
+//        robot.motorLiftL.setPower(llpower);
+//        robot.motorLiftR.setPower(rlpower);
+
+
+
+
+
+//      TELEMETRY
+//        telemetry.addData("Left Motor Power", gamepad1.left_stick_y);
+//        telemetry.addData("Right Motor Power", gamepad1.left_stick_y);
+//        telemetry.addData("Front Motor Power", gamepad1.left_stick_x);
+//        telemetry.addData("Back Motor Power", gamepad1.left_stick_x);
+//        telemetry.addData("Hook Motor Power", gamepad2.left_stick_y);
+//        telemetry.addData("Arm Tilt Motor Power", aPowerTIlt);
+//        telemetry.addData("Arm Ext Motor Power", aPowerExt);
+//        telemetry.addData("Box intake Power", boxPower);
+//        telemetry.addData("Right Distance", robot.sensorRangeR.getDistance(DistanceUnit.INCH));
+//        telemetry.addData("Left Distance", robot.sensorRangeL.getDistance(DistanceUnit.INCH));
+
+    }
+
+//--------------------------------- FUNCTIONS ----------------------------------------------------
+
+    public double scalePower(double power1)
+    {
+        double power2;
+        if(power1 > 0){
+            power2 = Math.pow(Math.abs(power1), 0.6);
+        } else if(power1 < 0){
+            power2 = -Math.pow(Math.abs(power1), 0.6);
+        } else{
+            power2 = 0;
+        }
+
+        return power2;
+    }
+}
