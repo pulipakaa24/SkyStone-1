9d1fe5996050ce27dde133f532ae8f0651199d0e
pulipakaa24 <63934255+pulipakaa24@users.noreply.github.com>
2020-08-12 19:15:18 -0500

    My week 1 hardware class assignment - dc motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Week1Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Week1Hardware.java
@@ -1,33 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevTouchSensor;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Week1Hardware {
     public DcMotor leftDrive = null;
     public DcMotor rightDrive = null;
 
-    public ColorSensor sensorColor;
-    public DistanceSensor sensorDistance;
-    public DigitalChannel digitalTouch;
-
     public static final double MAX_POWER = 0.99;
 
-    HardwareMap hwMap           =  null;
+    HardwareMap hwMap =  null;
 
     public void init(HardwareMap ahwMap) {
         hwMap = ahwMap;
 
-        leftDrive  = hwMap.get(DcMotor.class, "motor_l");
-        rightDrive  = hwMap.get(DcMotor.class, "motor_r");
-
-        sensorColor = hwMap.get(ColorSensor.class, "color_distance_sensor");
-        digitalTouch = hwMap.get(DigitalChannel.class, "touch_sensor");
-        sensorDistance = hwMap.get(DistanceSensor.class, "color_distance_sensor");
+        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive  = hwMap.get(DcMotor.class, "right_drive");
 
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -37,7 +25,5 @@ public class Week1Hardware {
 
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
     }
 }
